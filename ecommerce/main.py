64a9290b3e7c6f05bb40c85f143a9f710f048c4e
Ephy Mucira <ephymucira@gmail.com>
2024-10-16 11:01:19 +0300

    finished

--- ecommerce/main.py
+++ ecommerce/main.py
@@ -1,8 +1,34 @@
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 from models import *
+from authentications import *
+
+#signals
+from tortoise.signals import post_save
+from typing import List, Optional,Type
+from tortoise import BaseDBAsyncClient
 
 app = FastAPI()
+
+
+@post_save(User)
+async def create_business(
+    sender:"Type[User]",
+    instance:User,
+    created:bool,
+    using_db:"Optional[BaseDBAsyncClient]",
+    update_fields: List[str]
+
+) -> None:
+     if created:
+        business_obj = await Business.create(
+            business_name = instance.username, owner = instance
+        )
+
+        await business_pydantic.from_tortoise_orm(business_obj)
+
+        #sending the email
+
 
 @app.get("/")
 def index():
@@ -11,7 +37,15 @@ def index():
 
 
 @app.post("/register")
-async def user_registration(User)
+async def user_registration(user:user_pydanticIn):
+    user_info = user.dict(exclude_unset = True)
+    user_info["password"] = get_hashed_password(user_info["password"])
+    user_obj = await User.create(**user_info)
+    new_user = await user_pydantic.from_tortoise_orm(user_obj)
+    return {
+        "status":"ok",
+        "data":f"Hello {new_user.username}, thanks for choosingg our services, click the link to confirm registration."
+    }
 
 register_tortoise(
 
